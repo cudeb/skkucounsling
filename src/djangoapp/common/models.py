d8034ac830d8f55c3ff04d35b7c1d6c6d4e0273b
LeeGyuRi <lj01081512@gmail.com>
2023-11-13 03:43:37 +0900

    [BA-18] Auth user를 Common user로 변경

--- src/djangoapp/common/models.py
+++ src/djangoapp/common/models.py
@@ -1,19 +1,36 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-# from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수입니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
 
 class User(AbstractUser):
+    id = models.IntegerField(primary_key=True)
     user_type = models.CharField(max_length=20)
-    email = models.EmailField(unique=True) # 상담사는 임의 이메일 부여하여 생성
-    username = models.CharField(max_length=20, unique=False) # 실제 이름 ex) 정승혁
-    student_number =  models.CharField(max_length=20) # 학번, 상담사는 상담사 번호 부여
-    phone_number = models.CharField(max_length=20) # 연락처
-    birth = models.CharField(max_length=20) # 생년월일
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=20, unique=False)
+    student_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    birth = models.CharField(max_length=20)
+
+    objects = UserManager()
 
 class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE) # FK
     
 class Counselor(models.Model):
+    id = models.IntegerField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE) # FK
--- src/djangoapp/counseling/models.py
+++ src/djangoapp/counseling/models.py
@@ -3,31 +3,40 @@ from common.models import Student, Counselor
 # Create your models here.
 
 class CounselingApplication(models.Model):
+    id = models.IntegerField(primary_key=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE) # FK
     application_file =  models.FileField(upload_to="application", null=False)
     applied_at = models.DateTimeField(auto_now_add=True)
     counseling_type = models.CharField(max_length=30)
-    counseling_field = models.CharField(max_length=30)
     approved = models.BooleanField(default=False)
+    denied = models.BooleanField(default=False)
 
 
 class CounselingPrefertimeslot(models.Model):
+    id = models.IntegerField(primary_key=True)
     counseling_application = models.ForeignKey(CounselingApplication, on_delete=models.CASCADE) # FK
     timeslot = models.CharField(max_length=10)
 
+class CounselingPreferfield(models.Model):
+    id = models.IntegerField(primary_key=True)
+    counseling_application = models.ForeignKey(CounselingApplication, on_delete=models.CASCADE) # FK
+    field = models.CharField(max_length=100)
 
 class Counseling(models.Model):
+    id = models.IntegerField(primary_key=True)
     counseling_application = models.ForeignKey(CounselingApplication, on_delete=models.CASCADE) # FK
     student = models.ForeignKey(Student, on_delete=models.CASCADE) # FK
     counselor = models.ForeignKey(Counselor, on_delete=models.CASCADE,null=True) # FK
 
 
 class CounselingTestSchedule(models.Model):
+    id = models.IntegerField(primary_key=True)
     counseling = models.ForeignKey(Counseling, on_delete=models.CASCADE) # FK
     date = models.DateTimeField()
     timeslot = models.CharField(max_length=10)
 
 class CounselingSchedule(models.Model):
+    id = models.IntegerField(primary_key=True)
     counseling = models.ForeignKey(Counseling, on_delete=models.CASCADE) # FK
     session_date = models.DateField()
     session_timeslot = models.CharField(max_length=10)
@@ -36,5 +45,6 @@ class CounselingSchedule(models.Model):
 
 
 class CounselingJournals(models.Model):
-    counseling_schedule = models.ForeignKey(CounselingSchedule, on_delete=models.CASCADE) # FK
+    id = models.IntegerField(primary_key=True)
+    counseling_schedule = models.OneToOneField(CounselingSchedule, on_delete=models.CASCADE) # FK
     feedback = models.CharField(max_length=300)
