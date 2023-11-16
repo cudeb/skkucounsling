c5409ce374798c49103642044dbf7dc205a3727b
gimjinnyeong <wlssud0701@naver.com>
2023-11-16 16:57:52 +0900

    [FA-41] 학생상담 메인페이지 모달 부착

--- src/reactapp/src/components/modals/StudentCounselInfoModal.tsx
+++ src/reactapp/src/components/modals/StudentCounselInfoModal.tsx
@@ -1,5 +1,7 @@
 import {
   Button,
+  HStack,
+  IconButton,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,6 +16,7 @@ import {
 import { ICounselingStudent } from "../../dataflow/interface/counseling";
 import { useState } from "react";
 import { dateToKrLocaleWeekday } from "../../dataflow/DateFunc";
+import { CloseIcon } from "@chakra-ui/icons";
 
 const StudentCounselInfoModal = ({
   counsel,
@@ -35,15 +38,32 @@ const StudentCounselInfoModal = ({
     onClose();
   };
 
+  let [closeBtnOn, setCloseBtnOn] = useState<boolean>(false);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>
-          <Text fontSize="lg">상담 기록</Text>
-          <Text fontSize="md">상담 내용을 잊지 않도록 기록해보세요.</Text>
+          <HStack
+            style={{
+              width: "100%",
+            }}
+          >
+            <Text fontSize="lg">상담 기록</Text>
+            <Text fontSize="md">상담 내용을 잊지 않도록 기록해보세요.</Text>
+            <div style={{ flex: 1 }} />
+            <IconButton
+              icon={<CloseIcon />}
+              variant={"ghost"}
+              aria-label="Close"
+              onClick={() => {
+                setCloseBtnOn(true);
+              }}
+            />
+          </HStack>
         </ModalHeader>
-        <ModalCloseButton />
+
         <ModalBody>
           <VStack
             style={{
@@ -88,6 +108,41 @@ const StudentCounselInfoModal = ({
           </Button>
         </ModalFooter>
       </ModalContent>
+
+      <Modal
+        isOpen={closeBtnOn}
+        onClose={() => {
+          setCloseBtnOn(false);
+        }}
+        size="sm"
+      >
+        <ModalContent>
+          <ModalHeader />
+          <VStack>
+            <Text fontSize="xl" fontWeight="600">
+              저장하지 않고 나가시겠습니까?
+            </Text>
+            <HStack>
+              <Button
+                onClick={() => {
+                  setCloseBtnOn(false);
+                  onClose();
+                }}
+              >
+                예
+              </Button>
+              <Button
+                onClick={() => {
+                  setCloseBtnOn(false);
+                }}
+              >
+                아니오
+              </Button>
+            </HStack>
+          </VStack>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </Modal>
   );
 };
