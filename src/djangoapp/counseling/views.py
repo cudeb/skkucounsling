79fb591b5c0af42d5e4368da38dbbfa6a234c3a5
intyeger <intyeger@gmail.com>
2023-11-17 10:29:17 +0900

    [BA-28] CounselingJournalCounselor 오류 수정
    [request.GET -> request.data 수정] -> 취소
    1. 스케줄의 id를 받아서 해당 스케줄의 저널을 리턴하도록 수정
    2. 시리얼라이저 수정

--- src/djangoapp/counseling/views.py
+++ src/djangoapp/counseling/views.py
@@ -215,8 +215,9 @@ class CounselingJournalCounselor(APIView):
             res['error'] = "잘못된 요청입니다."
             return Response(res, status=status.HTTP_406_NOT_ACCEPTABLE)
         
-        counseling_journals = CounselingJournals.objects.get(counseling_schedule=schedule_id)
-        res['counseling_journals'] = CounselingScheduleSerializer(counseling_journals).data
+        counseling_schedule = CounselingSchedule.objects.get(id=schedule_id)
+        counseling_journals = CounselingJournals.objects.get(counseling_schedule=counseling_schedule)
+        res['counseling_journals'] = CounselingJournalsSerializer(counseling_journals).data
         
         return Response(res, status=status.HTTP_200_OK)
     
