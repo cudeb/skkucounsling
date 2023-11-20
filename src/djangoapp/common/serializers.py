280971693763d5b63b7ca74eb9a116fb9824286e
cudeb <kcrystalfg@naver.com>
2023-11-20 17:07:52 +0900

    [BA-38] User Serializer email 추가

--- src/djangoapp/common/serializers.py
+++ src/djangoapp/common/serializers.py
@@ -16,6 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = models.User
         fields = (
             "id",
+            "email",
             "user_type",
             "username",
             "realname",
